7eb3323896ebd65f4eca2a2f8341ac111bb02f7b
gregrent <gvillar39058@ucumberlands.edu>
2025-06-01 03:58:29 -0500

    Add recursive quick_sort calls and return result

--- quick_sort.py
+++ quick_sort.py
@@ -12,3 +12,5 @@ def quick_sort(arr):
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
