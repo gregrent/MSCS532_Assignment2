58f7022aaea04972dc63cdf51089f9b4e60e7dd6
gregrent <gvillar39058@ucumberlands.edu>
2025-06-01 03:57:47 -0500

    Add pivot selection and partitioning logic

--- quick_sort.py
+++ quick_sort.py
@@ -7,3 +7,8 @@
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
+    
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
