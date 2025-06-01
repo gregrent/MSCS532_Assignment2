686eb297f8ea1e2d6c35e91484dfb11fe29396c9
gregrent <gvillar39058@ucumberlands.edu>
2025-06-01 04:00:39 -0500

    Add comments and cleanup

--- quick_sort.py
+++ quick_sort.py
@@ -8,11 +8,19 @@ def quick_sort(arr):
     if len(arr) <= 1:
         return arr
     
+    # Choose the pivot element (here we choose the middle element)
     pivot = arr[len(arr) // 2]
+
+    # Partition the array into three sub-arrays:
+    # - elements less than pivot
+    # - elements equal to pivot
+    # - elements greater than pivot
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
 
+    # Recursively sort the left and right sub-arrays,
+    # then combine them with the middle part
     return quick_sort(left) + middle + quick_sort(right)
 
 arr = [10, 7, 8, 9, 1, 5]
