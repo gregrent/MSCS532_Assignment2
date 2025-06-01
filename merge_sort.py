9f2f0730f33823efa77d0eef4ee9cb99e78a96f6
gregrent <gvillar39058@ucumberlands.edu>
2025-06-01 03:51:18 -0500

    Add recursive splitting and merge function call in merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -9,5 +9,11 @@ def merge_sort(arr):
     if len(arr) <= 1:
         return arr
 
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    return merge(left_half, right_half)
+
 def merge(left, right):
     pass
