1227e717e91b9bd0088abd0588eed06d2bc0c8c6
gregrent <gvillar39058@ucumberlands.edu>
2025-06-01 03:47:14 -0500

    Initial commit with skeleton functions for both algorithms

---
+++ merge_sort.py
@@ -0,0 +1,12 @@
+#########################################
+# MSCS 532 Assignment 2
+# Author: Gregory Renteria
+# Merge Sort implementation.
+#########################################
+
+
+def merge_sort(arr):
+    pass
+
+def merge(left, right):
+    pass
---
+++ quick_sort.py
@@ -0,0 +1,8 @@
+#########################################
+# MSCS 532 Assignment 2
+# Author: Gregory Renteria
+# Quick Sort implementation.
+#########################################
+
+def quick_sort(arr):
+    pass
